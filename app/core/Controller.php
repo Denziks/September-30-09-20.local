c196332b873586b35dfcc02f7d6797c0f0b92e7e
Denzik <denzik.kornilov@gmail.com>
2022-10-02 22:20:10 +0300

    Filled class Controller which render view pages

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,5 +4,23 @@ namespace core;
 
 class Controller
 {
+    /**
+     * @var View
+     */
+    protected $view;
+
+    /**
+     * Controller constructor
+     */
+    public function __construct(){
+        $this -> view = new View();
+    }
+
+    /**
+     * rendering main page
+     */
+    public function index(){
+        $this -> view ->render('home');
+    }
 
 }
