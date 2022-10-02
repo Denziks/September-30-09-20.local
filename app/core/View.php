0195c2a09273dbbb1f2b6d14d50855df29976061
Denzik <denzik.kornilov@gmail.com>
2022-10-02 21:26:53 +0300

    Created class View which rendering templates

--- app/core/View.php
+++ app/core/View.php
@@ -3,5 +3,12 @@
 namespace core;
 class View
 {
-
+    /**
+     * rendering page by template
+     * @param string $page
+     * @param string $template
+     */
+    public function render(string $page, string $template = TEMPLATE_DEFAULT){
+        include_once 'app/resource/views/templates/'.$template.'.php';
+    }
 }
