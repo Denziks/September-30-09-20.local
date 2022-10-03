be376510098070faee20beb867a9d3f3b8aa03ca
Denzik <denzik.kornilov@gmail.com>
2022-10-03 14:07:28 +0300

    Created contacts page

---
+++ app/resources/views/pages/contacts.php
@@ -0,0 +1,25 @@
+<h2>
+    Contacts
+</h2>
+<div class="text">
+    <iframe src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+    </iframe>
+    <h3>
+        Address
+    </h3>
+    <p>
+        Venerable university with 38 colleges plus more than 22,000 students and 70 research departments.
+    </p>
+    <h3>
+        Phone number
+    </h3>
+    <p>
+        +32598466548
+    </p>
+    <h3>
+        Our contact manager
+    </h3>
+    <p>
+        John Snow
+    </p>
+</div>
