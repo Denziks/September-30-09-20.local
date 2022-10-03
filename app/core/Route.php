48d3d36dd992766a93d0e27b7487d1fa9918e1da
Denzik <denzik.kornilov@gmail.com>
2022-10-03 12:07:34 +0300

    Filled class Route which rendering templates

--- app/core/Route.php
+++ app/core/Route.php
@@ -4,4 +4,34 @@ namespace core;
 class Route
 {
 
+    /**
+     * return 404 code if page not found
+     */
+    static public function notFound(){
+        http_response_code(404);
+        exit();
+    }
+    /**
+     * initialisation of page by get param
+     */
+    static public function init(){
+        $action = 'index';
+        $actionInput = filter_input(INPUT_GET,'action');
+        if ($actionInput){
+            $action = $actionInput;
+        }
+        $controller = new Controller();
+        if (!method_exists($controller, $action)){
+            self::notFound();
+        }
+        $controller -> $action();
+    }
+    /**
+     * change get param of page by links
+     * @param $action
+     * @return string
+     */
+    static public function url($action){
+        return "/?action=$action";
+    }
 }
